d1ea13a2c29c3f544e4042c037bd08d5e041b773
adapiedrafita <ada.piedrafita.esteban@emeal.nttdata.com>
2023-10-19 22:03:40 +0200

    first commit

---
+++ src/test/java/com/adape/gtk/web/GtkCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adape.gtk.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtkCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
